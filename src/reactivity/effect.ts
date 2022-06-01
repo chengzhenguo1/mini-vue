ac5fe107bdc4ef7e0b91ec9ef188b6243cdf8844
guolicheng <guolicheng@tencent.com>
2022-06-01 15:14:26 +0800

    feat: effect和reactive依赖收集

---
+++ src/reactivity/effect.ts
@@ -0,0 +1,48 @@
+class ReactiveEffect {
+  private _fn: any
+
+  constructor(fn) {
+    this._fn = fn
+    activeEffect = this
+  }
+
+  run() {
+    this._fn()
+  }
+}
+
+let activeEffect
+
+// 收集依赖
+const targetMap = new Map()
+export function track(target, key) {
+  let depsMap = targetMap.get(target)
+  if (!depsMap) {
+    depsMap = new Map()
+    targetMap.set(target, depsMap)
+  }
+  
+  let deps = depsMap.get(key)
+  if (!deps) {
+    deps = new Set()
+    depsMap.set(key, deps)
+  }
+
+  deps.add(activeEffect)
+}
+
+export function trigger(target, key) {
+  const depsMap = targetMap.get(target)
+
+  const deps = depsMap.get(key)
+
+  for (let dep of deps) {
+    dep.run()
+  }
+}
+
+export function effect(fn) {
+  const _effect = new ReactiveEffect(fn)
+
+  _effect.run()
+}
--- src/reactivity/tests/reactive.spec.ts
+++ src/reactivity/tests/reactive.spec.ts
@@ -1,5 +1,16 @@
+import { reactive } from '../reactive'
+
 describe('reactive', () => {
   it('happy path', () => {
+    const user = {
+      name: 'zs'
+    }
 
+    const obj = reactive(user)
+
+    expect(obj.name).toBe('zs')
+
+    // 响应式包裹的对象不等于原始对象
+    expect(obj).not.toBe(user)
   })
 })
