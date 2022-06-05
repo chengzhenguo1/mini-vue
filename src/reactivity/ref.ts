aa519ab6dd2ef077803fda3a62571fafceaa71af
guolicheng <guolicheng@tencent.com>
2022-06-05 21:07:19 +0800

    feat: 实现isRef和unRef功能

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -6,7 +6,9 @@ class RefImpl {
   private _value: any
   // 以普通对象形式存储，value有可能是reactive对象，方便赋值的时候进行对比
   private rawValue: any
-  dep: Set<any>
+  // ref对象的标识符
+  public __v_isRef = true
+  public dep: Set<any>
 
   constructor(value) {
     this._value = convert(value)
@@ -37,4 +39,14 @@ export function convert(value) {
 
 export function ref(value) {
   return new RefImpl(value)
+}
+
+// 判断是否是ref对象
+export function isRef(ref) {
+  return !!ref.__v_isRef
+}
+
+// 取消ref对象，转换为普通对象
+export function unRef(ref) {
+  return isRef(ref) ? ref.value : ref
 }
--- src/reactivity/tests/ref.spec.ts
+++ src/reactivity/tests/ref.spec.ts
@@ -1,6 +1,6 @@
 import { effect } from "../effect";
 import { reactive } from "../reactive";
-import {  ref } from "../ref";
+import {  isRef, ref, unRef } from "../ref";
 // import { isRef, ref, unRef, proxyRefs } from "../ref";
 describe("ref", () => {
   it("happy path", () => {
@@ -40,21 +40,21 @@ describe("ref", () => {
     expect(dummy).toBe(2);
   });
 
-  // it("isRef", () => {
-  //   const a = ref(1);
-  //   const user = reactive({
-  //     age: 1,
-  //   });
-  //   expect(isRef(a)).toBe(true);
-  //   expect(isRef(1)).toBe(false);
-  //   expect(isRef(user)).toBe(false);
-  // });
+  it("isRef", () => {
+    const a = ref(1);
+    const user = reactive({
+      age: 1,
+    });
+    expect(isRef(a)).toBe(true);
+    expect(isRef(1)).toBe(false);
+    expect(isRef(user)).toBe(false);
+  });
 
-  // it("unRef", () => {
-  //   const a = ref(1);
-  //   expect(unRef(a)).toBe(1);
-  //   expect(unRef(1)).toBe(1);
-  // });
+  it("unRef", () => {
+    const a = ref(1);
+    expect(unRef(a)).toBe(1);
+    expect(unRef(1)).toBe(1);
+  });
 
   // it("proxyRefs", () => {
   //   const user = {
